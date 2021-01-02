f4090191cd046546a3e75659547618e4464bf653
Andreas <asc4asc@users.noreply.github.com>
2021-01-02 19:03:12 +0100

    Initial commit

---
+++ app1.js
@@ -0,0 +1,48 @@
+"use strict";  
+
+let tmp=document.querySelector("body");
+let tmp1=document.querySelector("clock");
+
+tmp.style.backgroundColor = "blue";
+
+(function () {
+	function uhrzeit() {
+		var jetzt = new Date(),
+			h = jetzt.getHours(),
+			m = jetzt.getMinutes(),
+			s = jetzt.getSeconds();
+		m = fuehrendeNull(m);
+		s = fuehrendeNull(s);
+		tmp1.innerHTML = h + ':' + m + ':' + s;
+		setTimeout(uhrzeit, 500);
+	}
+
+	function fuehrendeNull(zahl) {
+		zahl = (zahl < 10 ? '0' : '') + zahl;
+		return zahl;
+	}
+	document.addEventListener('DOMContentLoaded', uhrzeit);
+}());
+
+let i=1;
+document.body.addEventListener("click", event => {
+  if (i == 1) {
+    tmp.style.backgroundColor = "green"; i=0; window.navigator.vibrate(100);
+  } else {
+    tmp.style.backgroundColor = "yellow"; i=1; 
+    openFullscreen();
+  }
+});
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
